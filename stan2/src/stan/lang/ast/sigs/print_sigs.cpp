57d2c8b5dfaa17293978b9bb7e3f6560450270df
Sean Talts <sean.talts@gmail.com>
2018-10-29 17:45:26 -0400

    Move Rust proof of concept (RIP) and Stan 2 interface stuff

--- stan2/src/stan/lang/ast/sigs/print_sigs.cpp
+++
@@ -1,8 +0,0 @@
-#include <stan/lang/ast/sigs/function_signatures.hpp>
-
-#include <iostream>
-
-int main() {
-  stan::lang::function_signatures::instance().print_signatures(std::cout);
-  return 0;
-}
